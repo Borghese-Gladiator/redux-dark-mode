d2db84de6b39e0c6b243586592b239ee21536d1d
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-05-09 19:34:41 -0400

    create DarkThemeToggle component

---
+++ src/features/darkThemeToggle/DarkThemeToggle.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  toggle,
+  selectMode
+} from './darkThemeToggleSlice'; // actions
+import styles from './DarkThemeToggle.module.css';
+
+function DarkThemeToggle() {
+  const darkThemeEnabled = useSelector(selectMode);
+  const dispatch = useDispatch();
+
+  return (
+    <p>
+      <input
+        type="checkbox"
+        checked={darkThemeEnabled}
+        onChange={() => dispatch(toggle())}
+      ></input>
+      <span>Use Dark Theme</span>
+    </p>
+  );
+};
+
+export default DarkThemeToggle;
